1102e1a7615b79ab994529cae3581e25c2ae89b5
Dan Maxim <maxdanus@yahoo.com>
2020-07-20 12:37:12 -0600

    initial project setup

---
+++ src/Mx.Messaging.Api/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mx.Messaging.Models.Events;
+
+namespace Mx.Messaging.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Publish(ApplicationEvent eventModel)
+        {
+            return Ok("Received Event");
+        }
+    }
+}
---
+++ src/Mx.Messaging.Models/Events/ApplicationEvent.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Mx.Messaging.Models.Events
+{
+    public class ApplicationEvent
+    {
+        public string EventType { get; set; }
+
+        public string Body { get; set;  }
+
+        //public DateTime Sent { get; set; }
+    }
+}
